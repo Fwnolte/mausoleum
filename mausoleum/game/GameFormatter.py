262b0c4c5b2dcecce7bf039f3a2e57de39373091
Fred Nolte <noltefw@gmail.com>
2018-09-02 07:47:58 -0500

    Removed extra newline from print statements

--- mausoleum/game/GameFormatter.py
+++ mausoleum/game/GameFormatter.py
@@ -33,8 +33,7 @@ class GameFormatter:
     def print_new_room(self, environment):
         print_items = True if environment.items else False
 
-        print("\n")
-        print(environment.name)
+        print("\n" + environment.name)
         print("====================================")
         print(environment.description)
         if print_items:
@@ -44,27 +43,22 @@ class GameFormatter:
     def print_room_description(self, environment):
         print_items = True if environment.items else False
 
-        print("\n")
-        print(environment.description)
+        print("\n" + environment.description)
         if print_items:
             item_text = "The room contains " + self.make_item_list([item.reference_description for item in environment.items])
             print(item_text)
 
     def print_item_taken(self, item):
-        print("\n")
-        print("Took " + item.name + ".")
+        print("\nTook " + item.name + ".")
 
     def print_item_dropped(self, item):
-        print("\n")
-        print("Dropped " + item.name + ".")
+        print("\nDropped " + item.name + ".")
 
     def print_item_not_found(self, item_name):
-        print("\n")
-        print("There's no " + item_name + " around here...")
+        print("\nThere's no " + item_name + " around here...")
 
     def print_item_not_found_in_inventory(self, item_name):
-        print("\n")
-        print("There's no " + item_name + " in your inventory...")
+        print("\nThere's no " + item_name + " in your inventory...")
 
     def make_item_list(self, item_list):
         # TODO: Add underline formatting to words in item.reference_description that are usable to reference the item
